0c22a6b08eaa945db67ada2e9900c8e971563127
Corey Frenette <corey@narwhunderful.com>
2013-06-25 16:10:18 -0300

    Use async loading to ensure textures are loaded, and raise load event when Canvas is ready

--- src/graphics/gl00/Canvas.java
+++ src/graphics/gl00/Canvas.java
@@ -5,6 +5,7 @@ import graphics.shared.textures.Texture;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.IntBuffer;
+import java.util.LinkedList;
 
 import org.lwjgl.opengl.EXTFramebufferObject;
 import org.lwjgl.opengl.GL11;
@@ -14,6 +15,9 @@ public class Canvas {
   private Matrix _matrix = Context._matrix;
   private Texture _texture;
   
+  private Events _events = new Events(this);
+  private boolean _loaded;
+  
   private int _id;
   
   public Canvas(String name, int w, int h) {
@@ -21,11 +25,21 @@ public class Canvas {
     EXTFramebufferObject.glGenFramebuffersEXT(buffer);
     
     _id = buffer.get();
-    _texture = Context.getTextures().getTexture(name, w, h, null, true);
-    
-    bind();
-    EXTFramebufferObject.glFramebufferTexture2DEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, EXTFramebufferObject.GL_COLOR_ATTACHMENT0_EXT, GL11.GL_TEXTURE_2D, _texture.getID(), 0);
-    unbind();
+    _texture = Context.getTextures().getTexture(name, w, h, null);
+    _texture.events().addLoadHandler(new Texture.Events.Load() {
+      public void load() {
+        bind();
+        EXTFramebufferObject.glFramebufferTexture2DEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, EXTFramebufferObject.GL_COLOR_ATTACHMENT0_EXT, GL11.GL_TEXTURE_2D, _texture.getID(), 0);
+        unbind();
+        _loaded = true;
+        
+        _events.raiseLoad();
+      }
+    });
+  }
+  
+  public Events events() {
+    return _events;
   }
   
   public Texture getTexture() {
@@ -51,5 +65,38 @@ public class Canvas {
     _matrix.pop();
     _matrix.setProjection(_context.getW(), _context.getH(), false);
     EXTFramebufferObject.glBindFramebufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, 0);
+  }
+  
+  public static class Events {
+    private LinkedList<Load> _load = new LinkedList<Load>();
+    
+    private Canvas _this;
+    
+    public Events(Canvas data) {
+      _this = data;
+    }
+    
+    public void addLoadHandler(Load e) {
+      e._events = this;
+      _load.add(e);
+      
+      if(_this._loaded) {
+        raiseLoad();
+      }
+    }
+    
+    public void raiseLoad() {
+      for(Load e : _load) e.load();
+    }
+    
+    public static abstract class Load extends Event { public abstract void load(); }
+    
+    public static class Event {
+      protected Events _events;
+      
+      public void remove() {
+        _events._load.remove(this);
+      }
+    }
   }
 }
